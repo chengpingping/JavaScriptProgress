a6f78cadf95d61f6d21153819188693b43dd04d4
chengpingping <473398992@qq.com>
2019-12-22 23:32:58 +0800

    2019/12/22 commit

--- js/register.js
+++ js/register.js
@@ -1,24 +1,30 @@
 window.onload=function(){
 	var tel=/^1[3|4|5|7|8]\d{9}$/;//手机号码正则式
 	var qq=/^[1-9]\d{4,}$/;//QQ号正则表达式
+	var name=/^[\u4e00-\u9fa5]{2,8}$/;
 	var phone=document.querySelector('.phone');
 	var QQ=document.querySelector('.QQ');
+	var Name=document.querySelector('.name')
 	//tel
 	regExp(phone,tel);
 	//QQ
 	regExp(QQ,qq)
+	//昵称
+	regExp(Name,name);
+	
 	function regExp(element,regExp){
 		element.onblur=function(){
 			if(element.value==''){
-				element.nextSibling.nextSibling.innerHTML='请输入！';
+				console.log(element.nextSibling)
+				element.nextSibling.innerHTML='请输入！';
 				element.nextSibling.className='';
 				return 0;
 			}
-			if(regExp.test(phone.value)){
+			if(regExp.test(element.value)){
 				element.nextSibling.innerHTML='验证成功！';
 				element.nextSibling.className='right';
 			}else{
-				element.nextSibling.innerHTML='请输入有误！';
+				element.nextSibling.innerHTML='输入有误！';
 				element.nextSibling.className='wrong';
 			}
 		}
